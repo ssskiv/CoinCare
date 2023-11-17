195a4f238df81df59278c67ccfe022877dee4852
ssskiv <ivanioshpa@gmail.com>
2023-11-17 23:06:47 +0300

    123123

--- coincare/reg/forms.py
+++ coincare/reg/forms.py
@@ -5,28 +5,10 @@ from django.forms import TextInput, PasswordInput, EmailInput, CharField, EmailF
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
-
     username = CharField(widget=TextInput(attrs={'placeholder' :'Username'}))
     email = EmailField(widget=EmailInput(attrs={'placeholder' :'Email'}))
     password1 = CharField(label='Password',widget=PasswordInput(attrs={'placeholder' :'Password'}))
     password2 = CharField(label='Password confirmation',widget=PasswordInput(attrs={'placeholder' :'Password confirmation'}))
-
-    widgets = {
-        'username':TextInput(attrs={
-            'class':'form-control'
-        }),
-        'email':EmailInput(attrs={
-            'placeholder' :'Email', 
-            'class':'form-control'
-        }),
-        'password1':PasswordInput(attrs={
-            'class':'form-control'
-        }),
-        'password2':PasswordInput(attrs={
-            'class':'form-control'
-        })
-    }
     
     class Meta:
         model = User
