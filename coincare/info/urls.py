f5e370049b50890e37306488dd260e4cb2341d83
ssskiv <ivanioshpa@gmail.com>
2023-11-17 20:59:24 +0300

    asd

--- coincare/info/urls.py
+++ coincare/info/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name = 'index'),
-    path('register/', include('reg.urls'))
+    path('register/', include('reg.urls')),
+    path('login/', include('log.urls'))
 ]
--- coincare/log/views.py
+++ coincare/log/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from django.contrib.auth import authenticate, login
+
+from .forms import LoginForm
+
 # Create your views here.
+def login(request):
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request, 'info/index.html', form)
+
+        
